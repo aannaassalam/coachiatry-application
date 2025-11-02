634be57f38974edc24b8dc4d0281b258244d71b1
Salsaal shahid <salsaalshahid9038@gmail.com>
2025-11-02 12:22:58 +0530

    Downgraded

--- android/app/src/main/java/com/coachiatryapp/MainApplication.kt
+++ android/app/src/main/java/com/coachiatryapp/MainApplication.kt
@@ -1,27 +1,34 @@
 package com.coachiatryapp
 
 import android.app.Application
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.PackageList
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.soloader.SoLoader
 
-class MainApplication : Application(), ReactApplication {
+class MainApplication : Application() {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
+  // ✅ Lazy-loaded React Native host
+  val reactNativeHost: ReactNativeHost by lazy {
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> {
+        return PackageList(this).packages
+      }
+
+      override fun getJSMainModuleName(): String = "index"
+
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+    }
   }
 
   override fun onCreate() {
     super.onCreate()
-    loadReactNative(this)
-  }
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        load()
+    }
+}
+
 }
